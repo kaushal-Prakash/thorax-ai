a242ad62fbfb774c5532015d974159f17bfe146f
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-08-27 10:50:27 +0530

    added mongoDB service for db

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,21 @@ import e from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js"
 import 'dotenv/config';
+import connectDB from "./services/mongo.js";
 
 const app = e();
 
 app.use(e.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true, // Allow cookies to be sent
+}));
 
 //routes
 app.use("/auth",authRoutes);
+
+//connect to MongoDB
+connectDB();
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
---
+++ backend/services/mongo.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+};
+
+export default connectDB;
