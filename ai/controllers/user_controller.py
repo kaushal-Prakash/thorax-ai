f2d03c31efa590c3f8e90256e07c7592cdb2e83d
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-09-22 19:03:14 +0530

    cleaned ai backend

--- ai/controllers/user_controller.py
+++
@@ -1,7 +0,0 @@
-from services.db import db
-from helper.helper import serialize_user
-
-async def get_all_users():
-    users_cursor = db.users.find()
-    users = await users_cursor.to_list(length=None)
-    return [serialize_user(user) for user in users]
--- ai/main.py
+++ ai/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
-from routes import user
-from services.db import lifespan
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 # include routers
-app.include_router(user.router)
+# app.include_router(user.router)
 
 @app.get("/")
 def root():
-    return {"message": "AI backend is running"}
+    return {"message": "AI backend is running 🚀"}
 
 
